744fe9f99b37b3b898bf35d84d181a08448d0dd3
Tom Ross <tromik@gmail.com>
2016-12-22 00:16:33 -0500

    Exclamation product problem

---
+++ exproduct.py
@@ -0,0 +1,13 @@
+def product(s):
+    exclaim_cnt = 0
+    question_cnt = 0
+    for i in s:
+        if i == "!":
+            exclaim_cnt += 1
+        else:
+            question_cnt += 1
+    return exclaim_cnt * question_cnt
+
+print product('')
+print product('!')
+print product('!!??!!')
